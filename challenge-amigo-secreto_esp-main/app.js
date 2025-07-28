0413b6139724cfe6138a701ab98e52909cfb03cd
Marcelo Cattani <marcelocattani96@gmail.com>
2025-07-28 19:57:52 -0300

    add readme

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+let campoEntrada = document.querySelector("#amigo");
+
+function agregarAmigo() {
+  console.log(campoEntrada.value);
+}
